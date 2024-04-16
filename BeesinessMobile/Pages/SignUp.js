9ad11a32374aab2374682cbecd09e51754bcfb7b
grixxr <cambarijan.lancestuart@gmail.com>
2024-04-17 01:30:05 +0800

    datepicker fix

--- BeesinessMobile/Pages/SignUp.js
+++ BeesinessMobile/Pages/SignUp.js
@@ -35,10 +35,11 @@ export default function SignUp() {
     const [showDatePicker, setShowDatePicker] = useState(false);
     const onChange = (event, birthday) => {
         setShowDatePicker(false);
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
+        const year = birthday.getFullYear();
+        const month = String(birthday.getMonth() + 1).padStart(2, '0');
+        const day = String(birthday.getDate()).padStart(2, '0');
         const birthdate = `${year}-${month}-${day}`;
+        console.log(birthdate)
       };
     
     const backToLogin = () => {
